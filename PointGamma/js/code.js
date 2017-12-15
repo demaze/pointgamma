ce6161b9a50f68a08d645d151ff18b15c77795d4
demaze <34506798+demaze@users.noreply.github.com>
2017-12-15 14:44:57 +0100

    Add files via upload

--- PointGamma/js/code.js
+++ PointGamma/js/code.js
@@ -13,18 +13,15 @@ $(document).ready(function () {
                 if (largeur < 621 && large) {
                     large = false;
                     $("div.menuItem").css("display", "block");
-                    $("div.sousMenu").show();
                     $("div.menuItem").hide();
                     $("#logo").show();
                     $("#logo").css("position", "absolute");
                     $("#toggler").show();
-                    $("div.sousMenu").css("box-shadow","none");
 
                 }
                 if (largeur >= 621 && !large) {
                     large = true;
                     $("div.menuItem").css("display", "inline-block");
-                    $("div.sousMenu").hide();
                     $("div.menuItem").show();
                     $("#logo").css("position", "static");
                     $("#toggler").hide();
@@ -37,25 +34,5 @@ $(document).ready(function () {
         $("#logo").show();
 
     });
-
-
-
-    /* Le sous-menu des bars s'affiche
-     si on passe la souris dessus */
-
-
-    $("#bars").mouseenter(function () {
-        if (large) {
-            $(this).nextAll(".sousMenu").show();
-        }
-    });
-
-    $("#menubars").mouseleave(function () {
-        if (large) {
-            $(this).children(".sousMenu").hide();
-        }
-    });
-
-
 
 });
