e1a4085fe641cd7d7fcc71b08f32e83c1ece3fd0
matplaz <33354391+matplaz@users.noreply.github.com>
2018-01-06 19:41:48 +0100

    Create requestAJAXDesc.php

---
+++ PointGamma/utilities/requestAJAXDesc.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'dataGestion.php';
+$dbh = Database::connect();
+if (isset($_POST['desc']) && isset($_POST['barID'])) {
+    $desc = $_POST['desc'];
+    $barID = $_POST['barID'];
+    $query = "UPDATE Bars SET description=? WHERE id=?";
+    $sth = $dbh->prepare($query);
+    $request_succeeded = $sth->execute(array($desc, $barID));
+    if ($request_succeeded) {
+        echo "Enregistré";
+    } else {
+        echo "Erreur dans la mise à jour de la description";
+    }
+}
+
