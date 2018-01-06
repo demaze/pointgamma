4467b6933a543a47f2819baf365663615e5e19e8
matplaz <33354391+matplaz@users.noreply.github.com>
2018-01-06 19:39:36 +0100

    Update editinplace.js

--- PointGamma/js/editinplace.js
+++ PointGamma/js/editinplace.js
@@ -1,27 +1,22 @@
 $(document).ready(function () {
-    var uniqueDblclick = false;
-    $("#desc").bind("dblclick", function () {
-        if (!uniqueDblclick) {
-            OriginalText = $(this).html();
-            uniqueDblclick = true;
-            $(this).html("<form id='formDesc' method='post' action='content/content_bars_desc.php?todo=changeDesc'><input type='text' class='editBox' name='contentDesc' value='" + OriginalText + "' /><input type='submit' value='change' /> </form>");
-        }
-    });
-});
 
-$('body').delegate('#formDesc', 'submit', function (e) {
-    e.preventDefault();
-    var form = $(this);
-    var post_url = form.attr('action');
-    var post_data = form.serialize();
-    $.ajax({
-        type: 'POST',
-        url: post_url,
-        data: post_data,
-        success: function (msg) {
-            $(form).fadeOut(500, function () {
-                form.html(msg).fadeIn();
-            });
-        }
+
+    $("#submit").click(function () {
+       
+        
+        $.post(
+                'http://localhost/Eazy-J-master/PointGamma/utilities/requestAJAXDesc.php', // Un script PHP que l'on va créer juste après
+
+                {
+                    desc: $("#desc").val(), // Nous récupérons la valeur de nos inputs                
+                    barID: $("#barID").html()
+
+                },
+                function (data) {
+                    $("#success").html(data);
+                },
+                "text" // Nous souhaitons recevoir "Success" ou "Failed", donc on indique text !
+
+                );
     });
 });
