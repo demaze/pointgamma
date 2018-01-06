64dce1bee52a2df1dd5f27486ac367e77424bbe6
demaze <34506798+demaze@users.noreply.github.com>
2018-01-06 19:36:36 +0100

    Add files via upload

--- PointGamma/test.php
+++ PointGamma/test.php
@@ -1,18 +1,59 @@
-<html>
-    <head>
-        <link href="css/perso.css" rel="stylesheet">
-    </head>
-    <body>
-        <!--        <div class='menu' style='overflow: hidden; border:solid 10px green'>
-                    <img src='images/fond_rouge_100.jpg' style='position:fixed;top:0;left:0;height:100%;width:100%'>
-                    <p>lolololoololdozeofjoisejfouse</p>
+<?php
+require 'utilities/generation.php';
+require 'utilities/utils.php';
+generateHTMLHeader("lel");
+generateNavbar("lel");
+?>
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img class="first-slide" src="images/Deficio.jpg" alt="First slide">
+            <div class="container">
+                <div class="carousel-caption d-none d-md-block text-left">
+                    <h1>Example headline.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
                 </div>
-                <img src="images/logo.png" class="center animated fadeIn">-->
-
-
-        <div class="menu" style = "clip:rect(0px,10000px,100px,0px)">
-            <img src='images/fond_rouge_100.jpg' style='position:fixed;top:0;left:0;height:100%;width:100%'>
+            </div>
         </div>
+        <div class="carousel-item">
+            <img class="second-slide" src="images/Deficio.jpg" alt="Second slide">
+            <div class="container">
+                <div class="carousel-caption d-none d-md-block">
+                    <h1>Another example headline.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="third-slide" src="images/Deficio.jpg" alt="Third slide">
+            <div class="container">
+                <div class="carousel-caption d-none d-md-block text-right">
+                    <h1>One more for good measure.</h1>
+                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
 
-    </body>
-</html>
+<?php
+generateHTMLFooter()
+?>
