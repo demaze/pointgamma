1cf4cea89772ec1b7ad6ec3f458be4d191e1fec4
demaze <34506798+demaze@users.noreply.github.com>
2018-01-08 17:37:29 +0100

    Delete content_bars_desc.php

--- PointGamma/content/content_bars_desc.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../utilities/dataGestion.php';
-$dbh = Database::connect();
-if (isset($_GET['todo']) && $_GET['todo'] == "changeDesc") {
-    $desc = $_POST['contentDesc'];
-    $query = "UPDATE Bars SET description='$desc' WHERE id=?;";
-    $sth = $dbh->prepare($query);
-    $sth->execute(array($bar['id']));
-}
